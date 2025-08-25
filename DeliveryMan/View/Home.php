f2fd17bd60a915c90d80aa43e610d90d735707d4
AKib43 <akibash450@gmail.com>
2025-08-26 00:10:11 +0600

    Code base with Login page connections

---
+++ DeliveryMan/View/Home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <p>Welcome to delivery man Home Page</p>
+    </body>
+</html>
---
+++ HR/View/Home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <p>Welcome to HR Home Page</p>
+    </body>
+</html>
