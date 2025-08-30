872d0d7c24b6714ce5184a970c1e42fa73063c00
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2025-08-30 22:29:34 +0600

    Light/Dark mode for dashboard view

---
+++ HR/JS/script.js
@@ -0,0 +1,14 @@
+// dashboard view day/night mode
+
+const themeToggler = document.querySelector('.theme');
+
+themeToggler.addEventListener('click', () => {
+    
+    document.body.classList.toggle('dark');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+});
+
+//
