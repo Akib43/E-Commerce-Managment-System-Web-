db97ae2ae59c855bad1c349ca23bc099a81f3213
naimulislamsaikat <nzsaikat06@gmail.com>
2025-08-30 23:12:24 +0600

    approve leave requests of employees

---
+++ HR/View/leave_days.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
+
+<body>
+    <div class="container">
+        <!-- Top left -->
+        <header>
+            <div class="top">
+                <div class="logo">
+                    <h2>Hello, HR!</h2>
+                </div>
+            </div>
+
+            <!-- left panel option bar -->
+            <div class="sidebar">
+                <a href="#">
+                    <span class="material-symbols-outlined">grid_view</span>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">person</span>
+                    <h3>Profile</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">article_person</span>
+                    <h3>Employee Information</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">person_add_disabled</span>
+                    <h3>Add/Remove Employee</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">assessment</span>
+                    <h3>Edit Product List</h3>
+                </a>
+
+                <a href="#" class="active">
+                    <span class="material-symbols-outlined">event</span>
+                    <h3>Leave Management</h3>
+                    <span class="mgs_count">5</span>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">cast_for_education</span>
+                    <h3>Training & Development</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-outlined">logout</span>
+                    <h3>Logout</h3>
+                </a>
+            </div>
+
+        </header>
+
+        <!-- Top center -->
+        <main>
+            <div class="center">
+                <div class="top">
+                    <h1>Employee Leave Requests</h1>
+
+                    <!-- Employee Leave Requests list without db connection -->
+
+                </div>
+            </div>
+        </main>
+
+        <!-- Top right -->
+
+    </div>
+
+    <script src="../JS/script.js"></script>
+
+</body>
+
+</html>
