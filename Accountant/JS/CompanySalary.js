8722a625f96746a0d8248e7fb5131c9bcfbd7e20
Aki43 <akibah450@gmail.com>
2025-09-12 05:58:05 +0600

    Salary Table"

--- Accountant/JS/CompanySalary.js
+++ Accountant/JS/CompanySalary.js
@@ -1,25 +1,22 @@
-function salarytableview(eid, ename, edept, basesalary, employeeinc, employeeslary,  paydate) {
+function salarytableview(eID, ename, edept, baseSalary, incPercent, ammount){
+        var tbl = document.getElementById("SalaryTable");
 
-    var tbl = document.getElementById("expenseTable");
+        var row = document.createElement("tr");
+        row.innerHTML=`
+                <td>${eID}</td>
+                <td>${ename}</td>
+                <td>${edept}</td>
+                <td>${baseSalary}</td>
+                <td>${incPercent}</td>
+                <td>${ammount}</td>
+        `;
 
-    var row = document.createElement("tr");
-    row.innerHTML=`
-            <td>${eID}</td>
-            <td>${ename}</td>
-            <td>${ammount}</td>
-            <td>${edept}</td>
-            <td>${ereason}</td>
-            <td>${apdate}</td>
-            <td>${apname}</td>
-            <td>${rcdate}</td>
-            <td>${rcname}</td>
-    `;
+        tbl.appendChild(row);
 
-    tbl.appendChild(row);
+        row.addEventListener("click", () => {
+        alert("Adding to alert");
 
-    row.addEventListener("click", () => {
-    alert("Adding to alert");
+        });
 
-    });
+}
 
-    }
