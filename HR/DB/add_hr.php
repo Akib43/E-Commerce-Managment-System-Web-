2a0cc456931578bf4e6ef295f7105216e7c02bec
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-12 11:29:22 +0600

    hr db updated

--- HR/DB/add_hr.php
+++ HR/DB/add_hr.php
@@ -2,12 +2,12 @@
 include "config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = $_POST["name"];
-    $number = $_POST["number"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $address = $_POST["address"];
-    $joining = $_POST["joining"];
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $address = $_POST['address'];
+    $joining = $_POST['joining'];
 
     $sql = "INSERT INTO HR (Name, Number, Email, Password, Address, Joining) VALUES ('$name', '$number', '$email', '$password', '$address', '$joining')";
 
