bdefd1c123454a80ae9b1c90c57354815453afab
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-13 21:04:52 +0600

    add product db connection updat processing

--- HR/DB/add_product.php
+++ HR/DB/add_product.php
@@ -2,13 +2,16 @@
 include "config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST['id'];
     $name = $_POST['name'];
+    $category = $_POST['category'];
+    $type = $_POST['type'];
     $price = $_POST['price'];
-    $quantity = $_POST['quantity'];
+    $available = $_POST['available'];
+    $stock = $_POST['stock'];
     $details = $_POST['details'];
-    $warranty = $_POST['warranty'];
 
-    $sql = "INSERT INTO product (Name, Price, Quantity, Details, Warranty) VALUES ('$name', '$price', '$quantity', '$details', '$warranty',)";
+    $sql = "INSERT INTO `product_table`(`Product_ID`, `Product_Name`, `Product_Category`, `Product_Type`, `Product_Price`, `Product_Available`, `Product_Stocked`, `Product_Details_1`) VALUES ('[id]','[name]','[category]','[type]','[price]','[available]','[stock]','[details]')";
 
     if ($conn->query($sql) === FALSE) {
         echo "Failed: " . $conn->error;
