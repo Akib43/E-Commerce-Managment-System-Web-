84f5b74efa7774895df9ced2779a0d9e2db9dabe
YeaNipul <yeanipul34@gmail.com>
2025-09-14 01:48:36 +0600

    commit

--- DeliveryMan/db/deliverycardpull.php
+++ DeliveryMan/db/deliverycardpull.php
@@ -1,11 +1,14 @@
 <?php
 include 'start.php';
 
-$sql = "SELECT Order_ID, Order_Price, Order_Location, Order_City, Order_Type, Order_Product_Category, Order_Status, Customer_ID, Order_Date FROM Customer_Order_Table";
+$sql = "SELECT Order_ID, Order_Price, Order_Location, Order_City, Order_Type, Order_Product_Category, Order_Status, Customer_ID, Order_Date 
+FROM Customer_Order_Table";
 
-$result = $conn->query($sql)
 
-if ($result && $result->num_rows > 0) {
+
+        $result = $conn->query($sql)
+
+if($result && $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<script>
             orderview(
