3bdfd728834d7e7aae097eb8817528aeacbafab5
md-mridul-ali <mridulaiub17@gmail.com>
2025-09-14 04:33:30 +0600

    Lated Update

--- Accountant/PHP/SalaryTableShow.php
+++ Accountant/PHP/SalaryTableShow.php
@@ -2,11 +2,7 @@
 
 include 'ServerStart.php';
 
-<<<<<<< HEAD
 $sql = "SELECT Employee_ID, Employee_Name, Employee_Department, Employee_Base_Salary, Employee_Increment_Percent, Employee_Final_Salary, Last_Paid , Employee_Performance FROM employee_table";
-=======
-$sql = "SELECT Employee_ID, Employee_Name, Employee_Department, Employee_Base_Salary, Employee_Increment_Percent, Employee_Final_Salary, Last_Paid, Employee_Performance FROM employee_table";
->>>>>>> 68759c3640016379d6d59e04d2b8075778975297
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
