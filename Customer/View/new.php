3a1d2bfd4c60bd0e4e53759751b8628fceaaf0cb
md-mridul-ali <mridulaiub17@gmail.com>
2025-09-14 02:24:42 +0600

    user complete

--- Customer/View/new.php
+++ Customer/View/new.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_COOKIE["id"])) {
-    echo $_COOKIE["id"];
+    // echo $_COOKIE["id"];
     $id = $_COOKIE["id"];
     include '../PHP/config.php';
 
@@ -11,10 +11,12 @@ if (isset($_COOKIE["id"])) {
     if ($result->num_rows > 0) {
         while ($rows = $result->fetch_assoc()) {
             echo "<script>
-        document.getElementById('name').innerHTML = '{$rows['Customer_Name']}';
-        document.getElementById('email').innerHTML = '{$rows['Customer_Email']}';
-        document.getElementById('id').innerHTML = '{$rows['Customer_ID']}';
-    </script>";
+    document.getElementById('name').value = '" . $rows['Customer_Name'] . "';
+    document.getElementById('email').value = '" . $rows['Customer_Email'] . "';
+    document.getElementById('id').value = '" . $rows['Customer_ID'] . "';
+    document.getElementById('phn-no').value = '" . $rows['Customer_Number'] . "';
+    document.getElementById('address').value = '" . $rows['Customer_Address'] . "';
+</script>";
         }
         // header("Location:show.php");
     }
--- Customer/View/show.php
+++ Customer/View/show.php
@@ -7,19 +7,23 @@
     <link rel="stylesheet" href="../CSS/show.css">
 </head>
 <body>
-    <form action="" id="show-info">
+    <form action="../PHP/user_update.php" id="show-info" method="post">
         
         
         <label for="" style="margin-right: 63px;">Name:</label>
-        <input type="text" id="name" value=""><br>
+        <input type="text" id="name" name="name" value=""><br>
+
         <label for="" style="margin-right: 64px;">Email:</label>
-        <input type="email" id="email"><br>
+        <input type="email" name="email" id="email"><br>
+
         <label for="" style="margin-right: 85px;">ID:</label>
-        <input type="text" id="id" value="" readonly><br>
+        <input type="text" name="id" id="id" value="" readonly><br>
+
         <label for="" style="margin-right: 5px;">Phone Number:</label>
-        <input type="text" id="phn-no"><br>
+        <input type="text" name="number" id="phn-no"><br>
+
         <label for="" style="margin-right: 48px;">Address:</label>
-        <input type="text" id="address"><br>
+        <input type="text" name="address" id="address"><br>
         <div class="btn">
             <button type="submit" style="padding: 10px 120px; border-radius: 10px; background-color:blueviolet; color:#FFFFFF; font-size:larger;">Save Info</button>
         </div>
