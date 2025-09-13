993f60134a65568ab3fccb108e2863842d958790
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-13 06:53:57 +0600

    delete delivery man db connection

---
+++ HR/DB/delete_deliveryman.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $password = $_POST['password'];
+    $address = $_POST['address'];
+    $sql = "DELETE FROM Employee_Table WHERE Employee_Department='Delivery Man' AND Employee_ID='$id'";
+
+    if ($conn->query($sql) === FALSE) {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    } 
+    header("Location:../View/employee.php");
+    $conn->close();
+}
+
+?>
