b45186b8382df4b0e0c80ded077f629061f6d911
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-13 06:52:23 +0600

    config db updated

--- HR/DB/config.php
+++ HR/DB/config.php
@@ -2,7 +2,7 @@
 $host = "localhost"; //db er server ke store kora
 $username = "root"; // db username ke store kora
 $password = ""; // db password ke store kora
-$dbname = "e-commerce"; // db name
+$dbname = "e-commerce-managment-system"; // db name
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
