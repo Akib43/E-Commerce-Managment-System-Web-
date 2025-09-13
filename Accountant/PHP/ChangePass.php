182af3c8f7bad45eb592c6a058da02bf6bcc1621
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-14 04:50:36 +0600

    change pass update

--- Accountant/PHP/ChangePass.php
+++ Accountant/PHP/ChangePass.php
@@ -2,27 +2,28 @@
     include 'ServerStart.php';
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $currentpass = $_POST['currentpass'];
-        $newpass = $_POST['newpass'];
-        $confirmpass = $_POST['confirmpass'];
-        
-        $id = $_COOKIE['id'];
+        $currentpass = $_POST['old_pass'];
+        $newpass = $_POST['new_pass'];
+        $confirmpass = $_POST['confirm_pass'];
+
+        $id = $_SESSION['id'];
 
         if($newpass != $confirmpass) {
             echo "<script>alert('Pass Mismatch');</script>";
         } else {
-            $sql = "SELECT password FROM Employee_Table WHERE Employee_Id='$id'";
+            $sql = "SELECT Employee_password FROM Employee_Table WHERE Employee_Id='$id'";
             $result = $conn->query($sql);
             $row = $result->fetch_assoc();
-            if($row['password'] != $currentpass) {
-                echo "<script>alert('Current Password is incorrect.');</script>";
-            } else {
-                $sql = "UPDATE Employee_Table SET Employee_password='$newpass' WHERE Employee_Id='$id'";
-                if(mysqli_query($conn, $sql)) {
+            if($row['Employee_password'] == $currentpass) {
+                $sql1 = "UPDATE Employee_Table SET Employee_password='$newpass' WHERE Employee_Id='$id'";
+                if($conn->query($sql1) == TRUE) {
+                    header("Location: ../View/Profile.php");
                     echo "<script>alert(Password Changed);</script>";
                 } else {
                     echo "<script>alert('Password Not Set');</script>";
-                }
+                };
+            } else {
+                echo "<script>alert('Current Password is incorrect.');</script>";
             }
         }
     }
