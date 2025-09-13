8f92742a1f5dc20aa07ab2882c05953837657793
Aki43 <akibah450@gmail.com>
2025-09-14 02:58:06 +0600

    minor bugs fixed

--- Accountant/PHP/logintrigger.php
+++ Accountant/PHP/logintrigger.php
@@ -1,8 +1,7 @@
 <?php 
-
     include 'ServerStart.php';
     session_start();
-    $id = 1;
+    $id = 15;
     setcookie('id', $id, time() + (86400 * 30), "/");
 
     $sql = "SELECT * FROM Employee_Table WHERE Employee_Id = '$id'";
@@ -12,7 +11,7 @@
         while($row = $result->fetch_assoc()) {
             $_SESSION['name'] = $row['Employee_Name'];
             $_SESSION['email'] = $row['Employee_Email'];
-            $_SESSION['phone'] = $row['Employee_Phone'];
+            $_SESSION['phone'] = $row['Employee_Number'];
             $_SESSION['department'] = $row['Employee_Department'];
             $_SESSION['joiningdate'] = $row['Employee_Joining_Date'];
         }
