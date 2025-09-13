274fd15df73d6da1c1189efa7ffca52654c25e68
naimulislamsaikat <nzsaikat06@gmail.com>
2025-09-13 06:53:15 +0600

    delete customer db connection

---
+++ HR/DB/delete_customer.php
@@ -0,0 +1,22 @@
+<?php
+include 'config.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $password = $_POST['password'];
+    $address = $_POST['address'];
+    $joining = $_POST['joining'];
+    
+    $sql = "DELETE FROM customer_table WHERE 'Customer_ID='$id'";
+
+    if ($conn->query($sql) === FALSE) {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    } 
+    header("Location:../View/employee.php");
+    $conn->close();
+}
+
+?>
